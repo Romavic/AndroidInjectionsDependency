6af44280cfefe37d6f176a861f5d79ac825b80c3
Romavic dos Anjos <romavicdosanjoskc@gmail.com>
2021-10-02 23:24:49 +0100

    Koin DI configured on project.

---
+++ models/src/main/java/ao/romavicdosanjos/models/ModelsEverything.kt
@@ -0,0 +1,42 @@
+package ao.romavicdosanjos.models
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class ModelsEverything(
+    @SerialName("status")
+    var status: String? = null,
+    @SerialName("totalResults")
+    var totalResults: Int? = null,
+    @SerialName("articles")
+    var articles: MutableList<Article>? = null
+)
+
+@Serializable
+data class Article(
+    @SerialName("source")
+    var source: Source? = null,
+    @SerialName("author")
+    var author: String? = null,
+    @SerialName("title")
+    var title: String? = null,
+    @SerialName("description")
+    var description: String? = null,
+    @SerialName("url")
+    var url: String? = null,
+    @SerialName("urlToImage")
+    var urlToImage: String? = null,
+    @SerialName("publishedAt")
+    var publishedAt: String? = null,
+    @SerialName("content")
+    var content: String? = null
+)
+
+@Serializable
+data class Source(
+    @SerialName("id")
+    var id: String? = null,
+    @SerialName("name")
+    var name: String? = null
+)
